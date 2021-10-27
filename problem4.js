9ccfb00a98a5f159f21ecc4c390eaf66feecd04f
IsnainiFalihaAgustin <nenneniy47@gmail.com>
2021-10-27 11:54:49 +0700

    Day 2

--- problem4.js
+++ problem4.js
@@ -1,4 +1,27 @@
-// let alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+let result= ""
+let x
+let y
+let z
 
 function ubahHuruf(sentence) {
+
+    let ubah = sentence.split("")
+    for (let i = 0; i < sentence.length; i++) {
+
+        x = ubah[i].charCodeAt()+10
+
+        if (x > 90) {
+            y = x - 90
+            z = 64 + y
+            result += String.fromCharCode(z)
+
+        } else if (x - 10 == 32) {
+            result += String.fromCharCode(x-10)
+        } else {
+            result += String.fromCharCode(x)
+        }
+    }
+    return result
 }
+
+console.log(ubahHuruf("SEPULSA OKE"))     // COZEVCK YUO
